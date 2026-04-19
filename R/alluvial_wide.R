ba19145459249d563055f951217761d3b1251898
michaelcurry1123 <30420015+michaelcurry1123@users.noreply.github.com>
2026-04-19 13:33:46 -0400

    Implement warning for stratum_labels condition
    Add warning for missing labels when stratum_labels is FALSE

--- R/alluvial_wide.R
+++ R/alluvial_wide.R
@@ -382,6 +382,11 @@ alluvial_wide = function( data
       mutate(value_label = value)
     
   }
+    
+  if(!is.null(custom_value) && !stratum_labels){
+    warning("Warning with stratum_labels set to FALSE
+              no labels will be shown.")
+  }
   
   p <- ggplot(data_new,
               aes(x = x
